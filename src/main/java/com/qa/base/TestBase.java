385c83349f99f1f93cdc84d38e9394f538b61956
hp <hp@DESKTOP-8AK31QI>
2024-12-07 22:01:38 +0530

    update the TestBase Class

--- src/main/java/com/qa/base/TestBase.java
+++ src/main/java/com/qa/base/TestBase.java
@@ -1,8 +1,15 @@
 package com.qa.base;
 
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
 public class TestBase {
 	
+	WebDriver driver;
+	Properties prop;
 	public TestBase() {
+		
 		
 	}
 
