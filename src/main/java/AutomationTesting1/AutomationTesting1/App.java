f545d434b9dd6737a1e3170d5756e16c51389999
hp <hp@DESKTOP-8AK31QI>
2024-12-07 21:00:35 +0530

    FirstCommit

---
+++ src/main/java/AutomationTesting1/AutomationTesting1/App.java
@@ -0,0 +1,10 @@
+package AutomationTesting1.AutomationTesting1;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
